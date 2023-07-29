bf7309ef86005585210bd4733a81dc86ca40514d
Sabyasachi Paul <68630302+h0tPlug1n@users.noreply.github.com>
2023-07-29 22:57:08 +0530

    Add files via upload

--- scheduleDisplay.js
+++ scheduleDisplay.js
@@ -1,23 +1,46 @@
-// Getting Section Selection
-let currentSection = '';
-function setCurrentSection(section) {
-    // console.log(`current section : ${section}`);
-    currentSection = section;
-    
-    for (let i = 0; i < programs.children.length; i++) {
-        const section = programs.children[i].getAttribute("sec");
-        if (currentSection == section)
-            programs.children[i].classList.add("selected-section");
-        else
-            programs.children[i].classList.remove("selected-section");
-        console.log(section, programs.children[i]);
-    }
-}
-
-
+// REQUIRED VARIABLES
 const programs = document.getElementById("programs");
 const now = new Date();
 
+const sectionSelector = document.getElementById("select-section");
+const daySelector = document.getElementById("select-date")
+
+function showSchedule() {
+    const section = sectionSelector.value;
+    const day = daySelector.value;
+    for (let i = 0; i < programs.children.length; i++) {
+        const program = programs.children[i];
+
+        const programSection = program.getAttribute("sec");
+        const programDay = program.getAttribute("day");
+
+        if (section == programSection && day == programDay) {
+            program.classList.add("selected-section");
+        } else {
+            program.classList.remove("selected-section");
+        }
+    }
+}
+
+//=================================================================//
+// New
+// Getting Section Selection
+// let currentSection = '';
+// function setCurrentSection(section) {
+//     // console.log(`current section : ${section}`);
+//     currentSection = section;
+    
+//     for (let i = 0; i < programs.children.length; i++) {
+//         const section = programs.children[i].getAttribute("sec");
+//         if (currentSection == section)
+//             programs.children[i].classList.add("selected-section");
+//         else
+//             programs.children[i].classList.remove("selected-section");
+//         console.log(section, programs.children[i]);
+//     }
+// }
+//=================================================================//
+// OLD
 // Disabled for now
 
 // let i = 0;
